766355b4234588678fb7c6d643413d847a8a8d47
nlara99 <noahlara1999@gmail.com>
2024-08-31 18:17:46 -0600

    feat: :goal_net: Not Found Page
    - Adding page not found to App Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // .:|PAGE NOT FOUND|:.
+  {
+    path: '**',
+    title: 'Página no encontrada',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
